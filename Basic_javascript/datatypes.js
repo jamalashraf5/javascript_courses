5f957c97de8a759d23b3f2db21f46aea8497ff68
Jamal Ashraf <133765034+jamalashraf5@users.noreply.github.com>
2024-05-26 03:55:45 +0000

    datatypes added

---
+++ Basic_javascript/datatypes.js
@@ -0,0 +1,12 @@
+const id = Symbol('123');
+const anotherId = Symbol('123');
+// console.log(id === anotherId);
+
+const heroes = ["Thor", "Avenger","Spiderman"];
+// console.log(heroes);
+const dtls = {
+    name: "Jamal",
+    age: 24,
+    rollNo: 20
+};
+console.log(dtls.rollNo);
--- Basic_javascript/test.js
+++ Basic_javascript/test.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+// let score = undefined;
+// console.log(typeof score);
+
+// let valInNumber = Number(score);
+// console.log(typeof valInNumber);
+// console.log(valInNumber);
+
+let isLoggedIn = 97;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// let str1 = "Jamal";
+// let str2 = " Ashraf";
+// let str3 = str1 + str2;
+// let str4 = str1.concat(str2);
+// console.log(str3);
+// console.log(str4);
+
+console.log(1 + "3");
+console.log("3" + 4);
+console.log("3" + "4");
